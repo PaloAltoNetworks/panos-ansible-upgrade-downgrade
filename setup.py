81a679dd673edbb95f31ca58874813ffe3c2ee2b
Bora Mutluoglu <6238729-bmutluog@users.noreply.gitlab.com>
2021-03-19 20:54:20 +0000

    Added setup.py file for CI/CD

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='bps_automation',
+    version='0.1',
+    packages=['bps_automation'],
+    author='Dave Nguyen',
+    author_email='dnguyen@paloaltonetworks.com'
+)
