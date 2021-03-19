97280fa7556a6d4b27680fec007245d40e855423
Bora Mutluoglu <6238729-bmutluog@users.noreply.gitlab.com>
2021-03-19 21:56:10 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='bps_automation',
+    name='panos-ansible-upgrade-downgrade',
     version='0.1',
-    packages=['bps_automation'],
+    packages=['panos-ansible-upgrade-downgrade'],
     author='Dave Nguyen',
     author_email='dnguyen@paloaltonetworks.com'
 )
