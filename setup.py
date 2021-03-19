ee52f05f5fbde6c53ed536130a26168fc833050c
Bora Mutluoglu <6238729-bmutluog@users.noreply.gitlab.com>
2021-03-19 21:58:10 +0000

    Update setup.py to contain correct repository and author information for panos-ansible-upgrade-downgrade

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='panos-ansible-upgrade-downgrade',
     version='0.1',
     packages=['panos-ansible-upgrade-downgrade'],
-    author='Dave Nguyen',
-    author_email='dnguyen@paloaltonetworks.com'
+    author='Nathan Embery',
+    author_email='nembery@paloaltonetworks.com'
 )
