e09b6506207f2296ac1698904229747b908629fb
Nathan <nembery@gmail.com>
2021-06-22 13:43:50 -0400

    fix pipelines, add ansible syntax check and lint

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='panos-ansible-upgrade-downgrade',
-    version='0.1',
-    packages=['panos-ansible-upgrade-downgrade'],
-    author='Nathan Embery',
-    author_email='nembery@paloaltonetworks.com'
-)
